a5a3d61d3fe61b796518ba2bc2ccecaacc315a38
Golla <Mounika_Golla@Dell.com>
2019-03-13 10:26:57 -0500

    Appointment table changes

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,11 @@
-create table clients (
+CREATE TABLE IF NOT EXISTS clients (
 	id integer PRIMARY KEY AUTOINCREMENT,
 	name varchar(255),
 	phone_number varchar(255),
 	address varchar(255)
 );
 
-create table pets (
+CREATE TABLE IF NOT EXISTS pets (
 	id integer PRIMARY KEY AUTOINCREMENT,
 	name varchar(255),
 	gender varchar(255),
@@ -16,11 +16,23 @@ create table pets (
 );
 
 
-create table users (
+CREATE TABLE IF NOT EXISTS users (
 	id integer PRIMARY KEY AUTOINCREMENT,
 	username varchar(255),
 	encoded_password varchar(255),
 	role varchar(255)
 );
+
+CREATE TABLE IF NOT EXISTS appointments (
+  id INT NOT NULL AUTO_INCREMENT,
+  title varchar(255),
+  date date NOT NULL,
+  time time NOT NULL,
+  notes varchar(600),
+  client_id integer,
+  pet_id integer
+  FOREIGN KEY (client_id) REFERENCES clients(id)
+  FOREIGN KEY (pet_id) REFERENCES pets(id)
+)
 
 insert into users values (null, 'admin', 'password', 'SUPER_ADMIN');
