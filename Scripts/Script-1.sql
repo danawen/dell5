461f77016d0424823e388710c9537e2bebe38d23
Hauser, Matthew <Matthew_Hauser@Dell.com>
2019-03-29 16:04:50 -0500

    edit client changes

--- Scripts/Script-1.sql
+++ Scripts/Script-1.sql
@@ -1,16 +1,4 @@
-drop table appointments;
-
-create table appointments (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title NVARCHAR(255),
-    date DATE,
-    time TIME,
-    notes NVARCHAR(600),
-    client_id integer,
-  	pet_id integer,  
-  	FOREIGN KEY (client_id) REFERENCES clients(id)
-  	FOREIGN KEY (pet_id) REFERENCES pets(id)
-);
-
-
-INSERT INTO appointments(title, date, time, notes, client_id, pet_id) VALUES ('Vaccines for Scruffy','2019-03-26','09:00','Please apply cream after procedure',6,3);
+select * from clients;
+select * from pets;
+select * from appointments
+insert into appointments (title, date, time, notes) values ('test3','2019-03-02','14:00','test')
