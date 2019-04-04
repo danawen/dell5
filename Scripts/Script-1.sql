e64cb4e23dda1ee82f60289195659675fd16ef37
Hauser, Matthew <Matthew_Hauser@Dell.com>
2019-04-04 18:02:18 -0500

    added search for clients

--- Scripts/Script-1.sql
+++ Scripts/Script-1.sql
@@ -1,4 +1,1 @@
-select * from clients;
-select * from pets;
-select * from appointments
-insert into appointments (title, date, time, notes) values ('test3','2019-03-02','14:00','test')
+select * from clients where name like "joe"
